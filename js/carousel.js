5757901827e1f1dc91b6c821b4ea43c0094a4807
hannemann <hannemann666@gmx.net>
2022-01-22 11:29:54 +0100

    refactor carousel

--- js/carousel.js
+++ js/carousel.js
@@ -1,3 +1,10 @@
+const SELECTORS = {
+  controls: ".carousel-controls",
+  slides: ":scope > *",
+  btnBack: ":scope > span:first-of-type",
+  btnFwd: ":scope > span:last-of-type",
+};
+
 const CLASSNAMES = {
   prev: "prev",
   cur: "cur",
@@ -5,18 +12,29 @@ const CLASSNAMES = {
   moving: "moving",
 };
 
+const DIRECTIONS = {
+  back: -1,
+  fwd: 1,
+};
+
 class Carousel {
-  constructor(el, controls, duration) {
+  constructor(
+    el,
+    controlsClass,
+    options = { timingFunction: "ease-in-out", duration: 250 }
+  ) {
     this.el = el;
-    this.controls = controls;
-    if (duration) {
-      this.el.style.setProperty("--duration", `${parseInt(duration, 10)}ms`);
+    this.duration = options.duration;
+    this.timingFuntion = options.timingFunction;
+    this.initSlides();
+    if (this.slides.length > 1) {
+      this.initObserver();
+      this.controls = new controlsClass(this);
     }
-    this.initControls().initSlides();
   }
 
   initSlides() {
-    this.slides = Array.from(this.el.querySelectorAll(":scope > *"));
+    this.slides = Array.from(this.el.querySelectorAll(SELECTORS.slides));
     this.prev = this.slides.findIndex((s) =>
       s.classList.contains(CLASSNAMES.prev)
     );
@@ -30,66 +48,133 @@ class Carousel {
     return this;
   }
 
-  initControls() {
-    this.controls
-      .querySelector("span:first-of-type")
-      .addEventListener("click", this.back.bind(this));
-    this.controls
-      .querySelector("span:last-of-type")
-      .addEventListener("click", this.fwd.bind(this));
+  initObserver() {
+    this.observer = new MutationObserver((mutations) => {
+      mutations.forEach((m) => {
+        if (m.attributeName === "data-current") {
+          this.goto(parseInt(this.el.dataset.current, 10));
+        }
+      });
+    });
+
+    this.observer.observe(this.el, { attributes: true });
     return this;
   }
 
   back() {
     if (this.moving) return;
-    this.el.style.setProperty("--move", -1);
-    this.prepare();
-    this.cur = this.cur - 1 < 0 ? this.max : this.cur - 1;
-    this.prev = this.cur + 1 > this.max ? 0 : this.cur + 1;
-    this.next = this.cur - 1 < 0 ? this.max : this.cur - 1;
-    this.apply();
+    this.dir = DIRECTIONS.back;
+    this.move();
   }
 
   fwd() {
     if (this.moving) return;
-    this.el.style.setProperty("--move", 1);
-    this.prepare();
-    this.cur = this.cur + 1 > this.max ? 0 : this.cur + 1;
-    this.prev = this.cur - 1 < 0 ? this.max : this.cur - 1;
-    this.next = this.cur + 1 > this.max ? 0 : this.cur + 1;
-    this.apply();
+    this.dir = DIRECTIONS.fwd;
+    this.move();
+  }
+
+  move() {
+    this.prepare().setCur().setNext().setPrev().apply();
   }
 
   prepare() {
-    this.moving = true;
     this.slides[this.prev].classList.remove(CLASSNAMES.prev);
     this.slides[this.cur].classList.remove(CLASSNAMES.cur);
     this.slides[this.next].classList.remove(CLASSNAMES.next);
+    return this;
   }
 
   apply() {
-    this.slides[this.cur].addEventListener(
-      "transitionend",
-      () => (this.moving = false),
-      { once: true }
-    );
+    this.moving = true;
     this.slides[this.prev].classList.add(CLASSNAMES.prev);
     this.slides[this.cur].classList.add(CLASSNAMES.cur);
     this.slides[this.next].classList.add(CLASSNAMES.next);
   }
 
+  setCur(n) {
+    if (this.dir === DIRECTIONS.fwd) {
+      this.cur = this.cur + 1 > this.max ? 0 : this.cur + 1;
+    } else {
+      this.cur = this.cur - 1 < 0 ? this.max : this.cur - 1;
+    }
+    return this;
+  }
+
+  setNext() {
+    if (this.dir === DIRECTIONS.fwd) {
+      this.next = this.cur + 1 > this.max ? 0 : this.cur + 1;
+    } else {
+      this.next = this.cur - 1 < 0 ? this.max : this.cur - 1;
+    }
+    return this;
+  }
+
+  setPrev() {
+    if (this.dir === DIRECTIONS.fwd) {
+      this.prev = this.cur - 1 < 0 ? this.max : this.cur - 1;
+    } else {
+      this.prev = this.cur + 1 > this.max ? 0 : this.cur + 1;
+    }
+    return this;
+  }
+
+  set dir(d) {
+    this.el.style.setProperty("--move", d);
+  }
+
+  get dir() {
+    return parseInt(this.el.style.getPropertyValue("--move"));
+  }
+
   set moving(m) {
+    if (m) {
+      this.slides[this.cur].addEventListener(
+        "transitionend",
+        () => (this.moving = false),
+        { once: true }
+      );
+    }
     this.el.classList.toggle(CLASSNAMES.moving, m);
-    this.controls.classList.toggle(CLASSNAMES.moving, m);
+    this.controls.el.classList.toggle(CLASSNAMES.moving, m);
   }
 
   get moving() {
     return this.el.classList.contains(CLASSNAMES.moving);
   }
+
+  set duration(d) {
+    this.el.style.setProperty("--duration", `${parseInt(d, 10)}ms`);
+  }
+
+  get duration() {
+    return parseInt(this.el.style.getPropertyValue("--duration"), 10);
+  }
+
+  set timingFuntion(fn) {
+    this.el.style.setProperty("--timing-function", fn);
+  }
+
+  get timingFuntion() {
+    return this.el.style.getPropertyValue("--timing-function");
+  }
 }
 
-new Carousel(
-  document.querySelector(".carousel"),
-  document.querySelector(".carousel-controls"),
-  250
-);
+class CarouselControls {
+  constructor(carousel) {
+    this.el = document.querySelector(SELECTORS.controls);
+    this.carousel = carousel;
+    this.initListeners();
+  }
+
+  initListeners() {
+    this.el
+      .querySelector(SELECTORS.btnBack)
+      .addEventListener("click", () => this.carousel.back());
+    this.el
+      .querySelector(SELECTORS.btnFwd)
+      .addEventListener("click", () => this.carousel.fwd());
+    return this;
+  }
+}
+
+new Carousel(document.querySelector(".carousel"), CarouselControls);
