0852043d941d3e4814cda2c5a122fd5a2e1806ae
hannemann <hannemann666@gmx.net>
2022-01-27 22:00:42 +0100

    refactor: cleanup

--- js/carousel.js
+++ js/carousel.js
@@ -27,28 +27,17 @@ class Carousel {
     this.el = el;
     this.duration = this.defaultDuration = options.duration;
     this.timingFunction = this.defaultTimingFunction = options.timingFunction;
-    this.pointer = null;
-    this.initBounds().initSlides();
+    this.initSlides();
     if (this.slides.length > 1) {
-      this.initListeners().initObserver();
+      this.initPointer().initObserver();
       this.controls = new controlsClass(this);
     }
   }
 
   /**
-   * remember bounds of elements
-   * @returns {Carousel}
-   */
-  initBounds() {
-    this.bounds = {
-      el: this.el.getBoundingClientRect(),
-    };
-    return this;
-  }
-
-  /**
    * initialize slides
    * @returns {Carousel}
+   * @private
    */
   initSlides() {
     this.slides = Array.from(this.el.querySelectorAll(SELECTORS.slides));
@@ -65,53 +54,23 @@ class Carousel {
     return this;
   }
 
-  initListeners() {
-    this.el.addEventListener("pointerdown", (e) => {
-      e.preventDefault();
-      this.pointerStart = e.pageX;
-      this.el.style.setProperty('--transition-property', 'none');
-    });
-
-    this.el.addEventListener("pointermove", (e) => {
-      if (this.pointerStart) {
-        e.preventDefault();
-        const delta = e.pageX - this.pointerStart;
-        this.dir = delta > 0 ? DIRECTIONS.back : DIRECTIONS.fwd;
-
-        this.slides[this.prev].classList.remove(CLASSNAMES.prev);
-        this.slides[this.next].classList.remove(CLASSNAMES.next);
-        this.setPrev().setNext();
-        this.slides[this.prev].classList.add(CLASSNAMES.prev);
-        this.slides[this.next].classList.add(CLASSNAMES.next);
-  
-
-        let offset = this.dir === DIRECTIONS.fwd ? -100 : 100;
-        this.slides[this.prev].style.transform = `translateX(calc(${offset}% + (${delta}px)))`;
-        this.slides[this.cur].style.transform = `translateX(${delta}px)`;
-        offset = this.dir === DIRECTIONS.fwd ? 100 : -100;
-        this.slides[this.next].style.transform = `translateX(calc(${offset}% + (${delta}px)))`;
-      }
-    });
-
-    this.el.addEventListener("pointerup", (e) => {
-      e.preventDefault();
-      this.el.style.setProperty('--transition-property', '');
-      this.slides[this.prev].style.transform = '';
-      this.slides[this.cur].style.transform = '';
-      this.slides[this.next].style.transform = '';
-      if (e.pageX - this.pointerStart > 0) {
-        this.back();
-      } else {
-        this.fwd();
-      }
-      this.pointerStart = null;
-    });
+  /**
+   * initialize pointer events
+   * @returns {Carousel}
+   * @private
+   */
+  initPointer() {
+    this.pointerStart = null;
+    this.el.addEventListener("pointerdown", this.pointerDown.bind(this));
+    this.el.addEventListener("pointermove", this.pointerMove.bind(this));
+    this.el.addEventListener("pointerup", this.pointerUp.bind(this));
     return this;
   }
 
   /**
    * initialize observers
    * @returns {Carousel}
+   * @private
    */
   initObserver() {
     this.observer = new MutationObserver((mutations) => {
@@ -127,7 +86,53 @@ class Carousel {
   }
 
   /**
+   * handle pointerdown event
+   * @param {PointerEvent} e
+   * @private
+   */
+  pointerDown(e) {
+    e.preventDefault();
+    this.pointerStart = e.pageX;
+    this.transition = false;
+  }
+
+  /**
+   * handle pointer move event
+   * @param {PonterEvent} e
+   * @private
+   */
+  pointerMove(e) {
+    if (this.pointerStart) {
+      e.preventDefault();
+      const delta = e.pageX - this.pointerStart;
+      this.dir = delta > 0 ? DIRECTIONS.back : DIRECTIONS.fwd;
+      this.setPrev().setNext();
+      this.pxOffset = delta;
+    }
+  }
+
+  /**
+   * handle pointer up event
+   * @param {PointerEvent} e
+   * @private
+   */
+  pointerUp(e) {
+    e.preventDefault();
+    this.transition = true;
+    this.slides[this.prev].style.transform = "";
+    this.slides[this.cur].style.transform = "";
+    this.slides[this.next].style.transform = "";
+    if (e.pageX - this.pointerStart > 0) {
+      this.back();
+    } else {
+      this.fwd();
+    }
+    this.pointerStart = null;
+  }
+
+  /**
    * move backward
+   * @public
    */
   back() {
     if (this.moving) return;
@@ -137,6 +142,7 @@ class Carousel {
 
   /**
    * move forward
+   * @public
    */
   fwd() {
     if (this.moving) return;
@@ -147,12 +153,11 @@ class Carousel {
   /**
    * goto slide with index of n
    * @param {Number} n index of slide to go to
+   * @public
    */
   goto(n) {
     if (n !== this.cur) {
       this.dir = n > this.cur ? DIRECTIONS.fwd : DIRECTIONS.back;
-      this.oldDuration = this.duration;
-      this.oldTimingFunction = this.timingFunction;
       this.duration = 60;
       this.timingFunction = "linear";
       this.moveRecursive(n);
@@ -162,6 +167,7 @@ class Carousel {
   /**
    * recursivly move until desired index is reached
    * @param {Number} n goto slide with index of n
+   * @private
    */
   moveRecursive(n) {
     const next = this.dir === DIRECTIONS.fwd ? this.cur + 1 : this.cur - 1;
@@ -177,87 +183,76 @@ class Carousel {
       this.slides[this.cur].addEventListener(
         "transitionend",
         () => {
-          this.timingFunction = this.oldTimingFunction;
+          this.timingFunction = this.defaultTimingFunction;
         },
         { once: true }
       );
-      // last slide is next one
+      // this.step ist the last one so we ease-out the transition and reset the duration
       this.timingFunction = "ease-out";
-      this.duration = this.oldDuration;
+      this.duration = this.defaultDuration;
     }
     this.move();
   }
 
   /**
    * move slides
+   * @private
    */
   move() {
-    this.prepare().setCur().setNext().setPrev().apply();
-  }
-
-  /**
-   * remove classes from slides
-   * @returns {Carousel}
-   */
-  prepare() {
-    this.slides[this.prev].classList.remove(CLASSNAMES.prev);
-    this.slides[this.cur].classList.remove(CLASSNAMES.cur);
-    this.slides[this.next].classList.remove(CLASSNAMES.next);
-    return this;
-  }
-
-  /**
-   * apply classes to slides
-   * @returns {Carousel}
-   */
-  apply() {
-    this.moving = true;
-    this.slides[this.prev].classList.add(CLASSNAMES.prev);
-    this.slides[this.cur].classList.add(CLASSNAMES.cur);
-    this.slides[this.next].classList.add(CLASSNAMES.next);
+    this.setCur().setNext().setPrev();
   }
 
   /**
    * determine the cur slide
    * @returns {Carousel}
+   * @private
    */
   setCur() {
+    this.slides[this.cur].classList.remove(CLASSNAMES.cur);
     if (this.dir === DIRECTIONS.fwd) {
       this.cur = this.cur + 1 > this.max ? 0 : this.cur + 1;
     } else {
       this.cur = this.cur - 1 < 0 ? this.max : this.cur - 1;
     }
+    this.slides[this.cur].classList.add(CLASSNAMES.cur);
     return this;
   }
 
   /**
    * determine the next slide
    * @returns {Carousel}
+   * @private
    */
   setNext() {
+    this.slides[this.next].classList.remove(CLASSNAMES.next);
     if (this.dir === DIRECTIONS.fwd) {
       this.next = this.cur + 1 > this.max ? 0 : this.cur + 1;
     } else {
       this.next = this.cur - 1 < 0 ? this.max : this.cur - 1;
     }
+    this.slides[this.next].classList.add(CLASSNAMES.next);
     return this;
   }
 
   /**
    * determine the prev slide
    * @returns {Carousel}
+   * @private
    */
   setPrev() {
+    this.slides[this.prev].classList.remove(CLASSNAMES.prev);
     if (this.dir === DIRECTIONS.fwd) {
       this.prev = this.cur - 1 < 0 ? this.max : this.cur - 1;
     } else {
       this.prev = this.cur + 1 > this.max ? 0 : this.cur + 1;
     }
+    this.slides[this.prev].classList.add(CLASSNAMES.prev);
     return this;
   }
 
   /**
    * @param {Number} d 1: fwd, -1: back
+   * @private
    */
   set dir(d) {
     this.el.style.setProperty("--move", d);
@@ -265,6 +260,7 @@ class Carousel {
 
   /**
    * @returns {Number}
+   * @private
    */
   get dir() {
     return parseInt(this.el.style.getPropertyValue("--move"));
@@ -272,6 +268,7 @@ class Carousel {
 
   /**
    * @param {Boolean} m
+   * @private
    */
   set moving(m) {
     if (m) {
@@ -287,6 +284,7 @@ class Carousel {
 
   /**
    * @returns {Boolean}
+   * @private
    */
   get moving() {
     return this.el.classList.contains(CLASSNAMES.moving);
@@ -294,6 +292,7 @@ class Carousel {
 
   /**
    * @param {NUmber} d transition duration in ms
+   * @private
    */
   set duration(d) {
     this.el.style.setProperty("--duration", `${parseInt(d, 10)}ms`);
@@ -301,6 +300,7 @@ class Carousel {
 
   /**
    * @returns {Number}
+   * @private
    */
   get duration() {
     return parseInt(this.el.style.getPropertyValue("--duration"), 10);
@@ -308,6 +308,7 @@ class Carousel {
 
   /**
    * @param {String} fn string representing the timing function
+   * @private
    */
   set timingFunction(fn) {
     this.el.style.setProperty("--timing-function", fn);
@@ -315,9 +316,45 @@ class Carousel {
 
   /**
    * @returns {String}
+   * @private
    */
   get timingFunction() {
     return this.el.style.getPropertyValue("--timing-function");
+  }
+
+  /**
+   * @param {Boolean} t
+   * @private
+   */
+  set transition(t) {
+    t = typeof t === "boolean" && t;
+    this.el.style.setProperty("--transition-property", t ? "" : "none");
+  }
+
+  /**
+   * @returns {Boolean}
+   * @private
+   */
+  get transition() {
+    return this.el.style.getPropertyValue("--transition-property") !== "none";
+  }
+
+  /**
+   * set pixel based offset
+   * @param {Number} px number of pixels
+   * @private
+   */
+  set pxOffset(px) {
+    // hint: prev and next exchange their position if direction changes
+    const offsetPrev = this.dir === DIRECTIONS.fwd ? -100 : 100;
+    const offsetNext = this.dir === DIRECTIONS.fwd ? 100 : -100;
+    this.slides[
+      this.prev
+    ].style.transform = `translateX(calc(${offsetPrev}% + (${px}px)))`;
+    this.slides[this.cur].style.transform = `translateX(${px}px)`;
+    this.slides[
+      this.next
+    ].style.transform = `translateX(calc(${offsetNext}% + (${px}px)))`;
   }
 }
 
@@ -336,8 +373,8 @@ class CarouselControls {
       .querySelector(SELECTORS.btnFwd)
       .addEventListener("click", () => this.carousel.fwd());
     this.el.querySelectorAll(SELECTORS.btnGoto).forEach((b) => {
-      b.addEventListener("click", (e) => {
-        this.carousel.el.dataset.current = b.dataset.slide;
+      b.addEventListener("click", () => {
+        this.carousel.goto(parseInt(b.dataset.slide));
       });
     });
     return this;
